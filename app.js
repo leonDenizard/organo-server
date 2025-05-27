a24b4991fb22160c7c00a233923ffdd13e052c37
leonDenizard <de.nizard@hotmail.com>
2025-05-27 20:02:04 -0300

    Fix: ajustado cors para testar delete na vercel

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const userRoute = require('./src/routes/user.route');
 const scheduleRoute = require('./src/routes/schedule.route');
 
 const cors = require('cors');
+
 const connectDatabase = require('./src/database/db');
 const Schedule = require('./src/models/Schedule');
 
@@ -10,28 +11,34 @@ const app = express();
 
 connectDatabase();
 
-app.use(express.json({ limit: '50mb' }));
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
-
-const allowedOrigins = [
-  'http://localhost:3000',
-  'https://organo-two-henna.vercel.app'
-];
-
 const corsOptions = {
   origin: function (origin, callback) {
+    console.log('Requisição recebida de:', origin);
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
+      console.log('⚠️ Origin não permitida:', origin);
       callback(new Error('Not allowed by CORS'));
     }
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   credentials: true,
 };
-
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions));
+
+
+
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
+
+const allowedOrigins = [
+  'http://localhost:3000',
+  'http://localhost:5174',
+  'https://organo-two-henna.vercel.app',
+  'server-ten-ivory.vercel.app'
+];
+
 
 app.use('/api/user', userRoute);
 app.use('/api/schedule', scheduleRoute);
